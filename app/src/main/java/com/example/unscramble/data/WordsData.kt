7dc35f7acd28f3a72671343570adfe3cc685fa54
Annelisebx <50610153+Annelisebx@users.noreply.github.com>
2023-12-02 22:36:36 -0800

    Initial commit

---
+++ app/src/main/java/com/example/unscramble/data/WordsData.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.unscramble.data
+
+const val MAX_NO_OF_QUESTIONS = 5
+const val SCORE_INCREASE = 1
+
+// Set with all the words for the Game
+val allQuestions: Set<String> =
+    setOf(
+        "Favorite Color?",
+        "Favorite animal?",
+        "First Job?",
+        "Birthday?",
+        "Favorite sport?",
+        "Where did I go to college?",
+        "Favorite breakfast food?",
+        "Favorite dinner food?",
+        "Beach or Mountains?",
+        "Favorite board game?",
+        "Favorite place to eat?",
+        "Favorite holiday?"
+    )
